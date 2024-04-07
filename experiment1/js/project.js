eb59cc3d5f70947438427ca9d475f8d34d1df779
Qizhe Lao <qlao@ucscl.com>
2024-04-07 00:41:11 -0700

    imported to web

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,5 +1,5 @@
 // project.js - purpose and description here
-// Author: Your Name
+// Author: Qizhe Lao
 // Date:
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
@@ -23,12 +23,63 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  const fillers = {
+    adventurer: ["slug"],
+    location: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
+    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
+    item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
+    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
+    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
+    loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
+    baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
+    message: ["work"],
+    
+  };
+  
+  const template = `$adventurer, heed my $message!
+  
+  I have just come from $location where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
+  
+  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+    // box.innerText = story;
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  // clicker.onclick = generate;
+  
+  generate();
 
-  // call a method on the instance
-  myInstance.myMethod();
+    // // create an instance of the class
+  // let myInstance = new MyProjectClass("value1", "value2");
+
+  // // call a method on the instance
+  // myInstance.myMethod();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
