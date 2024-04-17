a94cc2727e39ff1ee28aebadc0d2ebeb5b5c7780
Qizhe Lao <qlao@ucscl.com>
2024-04-16 21:59:30 -0700

    ex2 finish

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -14,6 +14,12 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
+let seed = 0;
+let topcolor, bottomcolor, n;
+let time = 0; // time variable
+let noiseScale = 0.01;
+let Mstart = 300;
+let Mend = 462;
 
 class MyClass {
     constructor(param1, param2) {
@@ -32,9 +38,11 @@ function resizeScreen() {
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
+  $("#reimagine").click(function() {seed++;});
 }
 
 // setup() function is called once when the program starts
+
 function setup() {
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
@@ -53,27 +61,64 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
+  randomSeed(seed);
+  noiseSeed(seed);
+  // background
+  topcolor = color(19, 20, 25);
+  bottomcolor = color(46, 57, 79)
+  for(let y = 0; y < height; y++) {
+    n = map(y, 0, 250, 0, 1);
+    let newcolor = lerpColor(topcolor, bottomcolor, n);
+    stroke(newcolor);
+    line(0, y, width, y);
+  }
+  
+  // star
   fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
+  color(0);
+  const Stars = 600;
+  for(let i = 0; i < Stars; i++) {
+    this.x = random(width);
+    this.y = random(400);
+    this.size = random(0.5, 3);
+    ellipse(this.x, this.y, this.size);
+  }
+  
+  // mountain
+  fill(0);
+  beginShape();
+  for (let x = 0; x < width; x++) {
+    let noiseVal = noise(x * noiseScale) * (Mend - Mstart);
+    vertex(x, Mend - noiseVal);
+  }
+  vertex(width, Mend); 
+  vertex(0, Mend); 
+  endShape(CLOSE); 
+  
+  // Lake
+  fill(7, 17, 27);
+  noStroke();
+  rect(0, height / 1.3, width, height + 1);
+  
+  // wave
+  translate(0, height / 1.25) 
+  // make a x and y grid of ellipses
+  fill(74, 82, 113);
+  noStroke();
+  for (let x = 0; x <= width; x = x + 20) {
+    for (let y = 0; y <= height-200; y = y + 20) {
+      // starting point of each circle depends on mouse position
+      const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
+      const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
+      // and also varies based on the particle's location
+      const angle = xAngle * (x / width) + yAngle * (y / height);
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+      // each particle moves in a circle
+      const myX = x + 10 * cos(2 * PI * time + angle);
+      const myY = y + 10 * sin(2 * PI * time + angle);
+
+      ellipse(myX, myY, 10); // draw particle
+    }
+  }
+  time = time + 0.005; // update time
 }
